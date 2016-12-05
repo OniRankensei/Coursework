97c587739ecbc63688a04e05f47bd81112f67e08
OniRankensei <oni.rankensei@live.com>
2016-12-04 22:17:11 -0800

    Added Final via upload
    This is the most current version of the project. The most recent change was the addition to continue the game for as long as the user chose. There were also some minor changes such as the winning message, the addition of a "inProgress" instance variable and some formatting of the code for increased readability.

--- Final Project/src/FinalV3.java
+++ Final Project/src/FinalV3.java
@@ -2,83 +2,113 @@ import java.util.*;
 import java.io.*;
 import java.lang.reflect.Array;
 
-public class FinalV2 {
+public class FinalV3 {
 
 	public static void main(String[] args) {
 
-		TicTacToeV2 game = new TicTacToeV2();
+		Scanner input = new Scanner(System.in);
+
+		TicTacToeV3 game = new TicTacToeV3();
 
 		game.spashScreen();
-
 		game.askForUserName();
 
-		game.setBoard();
+		do {
 
-		while (game.on) {
+			game.setBoard();
 
-			game.displayBoard();
-			game.playerMakeMove();
-			game.checkForWin(game.currentPlayer);
-			game.changePlayer();
-			game.changeTurn();
+			while (game.inProgress) {
 
-		}
+				game.displayBoard();
+				game.playerMakeMove();
+				game.checkForWin(game.currentPlayer);
+				game.changePlayer();
+				game.changeTurn();
 
-	} // end of main
+			}
 
-} // end of FinalV2
+			game.checkWhetherToContinue();
+			game.resetGame();
 
-class TicTacToeV2 {
+		} while (game.on);
+
+		input.close();
+
+	}
+
+}
+
+class TicTacToeV3 {
 
 	// Scanner object created for user input
 	Scanner input = new Scanner(System.in);
 
-	// final size of the array
-	public final int SIZE_OF_ARRAY = 9;
-	public boolean on;
+	public String username;
+	public final int SIZE_OF_ARRAY = 9; // size of the board
+	public boolean on; // will control whether to continue playing
+	public boolean inProgress; // will control current game
 	public final String HUMAN_PLAYER = "H";
 	public final String COMPUTER_PLAYER = "C";
 	public String currentPlayer;
 	public int currentTurn;
 	public final int NUMBER_OF_TURNS = 9;
 
-	// testing
-	boolean condition;
-	boolean[] winningConditions;
-
-	// creation of a String array with a size of 9
+	// creation of a String array with a size of 9 serving as the board
 	String[] array = new String[SIZE_OF_ARRAY];
 
-	public String username;
+	/**
+	 * Default constructor. It sets the current player to the human player and
+	 * sets the current turn to the first turn.
+	 */
 
-	// Conthis.arrayuctor
-	public TicTacToeV2() {
+	public TicTacToeV3() {
 		this.on = true;
+		this.inProgress = true;
 		currentPlayer = HUMAN_PLAYER;
 		currentTurn = 1;
 
 	}
 
 	/**
+	 * Resets the game and changes the currentPlayer to the human player.
+	 */
+
+	public void resetGame() {
+
+		this.inProgress = true;
+		this.currentPlayer = HUMAN_PLAYER;
+		this.currentTurn = 1;
+	}
+
+	/**
 	 * Populates the array with the numbers 0 through 8 as Strings.
 	 */
+
 	public void setBoard() {
 		for (int i = 0, j = 0; i < SIZE_OF_ARRAY; i++, j++) {
 			this.array[i] = "" + j;
 		}
 	}
-	
+
+	/**
+	 * Changes the currentTurn. If the currentTurn exceeds the maximum
+	 * NUMBER_OF_TURNS, the game is declared as a tie and the current game in
+	 * progress ends.
+	 */
+
 	public void changeTurn() {
-		
+
 		this.currentTurn++;
-		System.out.println("The current turn is " + currentTurn);
-		
-		if(currentTurn > NUMBER_OF_TURNS) {
+
+		if (currentTurn > NUMBER_OF_TURNS) {
 			exit();
-			//displayBoard();
-			System.out.print("The game is a tie!");
+			System.out.print("The game is a tie!\n");
 		}
 	}
+
+	/**
+	 * Displays the board in its current state.
+	 */
 
 	public void displayBoard() {
 
@@ -89,18 +119,25 @@ class TicTacToeV2 {
 		System.out.println(" |-----|");
 	}
 
+	/**
+	 * Allows the current player to make a selection of which space to occupy in
+	 * the board. The selection is validated to ensure that the move is a legal
+	 * move by verifying that the choice is one space in the board and that the
+	 * selection has not been selected by the other player.
+	 */
+
 	public void playerMakeMove() {
 
 		int indexSelected;
 		String userInput;
 
 		if (currentPlayer.equals(HUMAN_PLAYER)) {
-			System.out.println("It is the human's turn.");
+			System.out.printf("\nIt is %s's turn.\n", this.username);
 		} else if (currentPlayer.equals(COMPUTER_PLAYER)) {
-			System.out.println("It is the computer's turn.");
+			System.out.println("\nIt is the computer's turn.");
 		}
 
-		System.out.println("Choose a space.");
+		System.out.println("Please choose a space.");
 		userInput = input.nextLine();
 
 		indexSelected = validatePlayersMove(userInput);
@@ -109,7 +146,19 @@ class TicTacToeV2 {
 
 		this.array[indexSelected] = currentPlayer;
 
-	} // end of playerMakeMove
+	}
+
+	/**
+	 * Checks for position availability by verifying whether the specified
+	 * position has already been selected by either player.
+	 * 
+	 * @param indexSelected
+	 *            - an integer that represents a player's choice for the space
+	 *            it chooses to occupy.
+	 * 
+	 * @return - the player's choice of space if it has not been already chosen
+	 *         by either player.
+	 */
 
 	public int checkPositionAvailability(int indexSelected) {
 		String userInput;
@@ -121,6 +170,7 @@ class TicTacToeV2 {
 			userInput = input.nextLine();
 			indexSelected = validatePlayersMove(userInput);
 		}
+
 		return indexSelected;
 	}
 
@@ -146,7 +196,12 @@ class TicTacToeV2 {
 		System.out.println("Press <ENTER> to continue . . . ");
 		input.nextLine();
 
-	} // end of spashScreen method
+	}
+
+	/**
+	 * Asks for the user's name and will only accept a name that consists of
+	 * alphabetic characters without any spaces.
+	 */
 
 	public void askForUserName() {
 
@@ -164,57 +219,166 @@ class TicTacToeV2 {
 
 	}
 
+	/**
+	 * Changes the current player to the opposite character, that is, if the
+	 * current player is the human player, then it sets the current player to
+	 * the computer player and vice-versa.
+	 */
+
 	public void changePlayer() {
 
 		if (currentPlayer.equals(HUMAN_PLAYER)) {
+
 			currentPlayer = COMPUTER_PLAYER;
-			// System.out.println("It is the computer's turn.");
+
 		} else if (currentPlayer.equals(COMPUTER_PLAYER)) {
+
 			currentPlayer = HUMAN_PLAYER;
-			// System.out.println("It is the human's turn.");
+
 		}
 	}
 
-	public void checkForWin(String str) {
+	/**
+	 * Checks the current player's choices on the board to see whether the
+	 * current player has won the game.
+	 * 
+	 * @param currentPlayer - the current player.
+	 *            -
+	 */
 
-		String player;
+	public void checkForWin(String currentPlayer) {
 
-		if (str.equals("H")) {
-			player = "human";
+		String winningMessage;
+
+		if (currentPlayer.equals("H")) {
+
+			winningMessage = username + " has won the game!";
+
 		} else {
-			player = "computer";
+
+			winningMessage = "The computer has won the game!";
 		}
 
+		// row 1 winning condition
 		if (array[0].equals(currentPlayer) && array[1].equals(currentPlayer) && array[2].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// row 2 winning condition	
 		} else if (array[3].equals(currentPlayer) && array[4].equals(currentPlayer) && array[5].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// row 3 winning condition	
 		} else if (array[6].equals(currentPlayer) && array[7].equals(currentPlayer) && array[8].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// column 1 winning condition
 		} else if (array[0].equals(currentPlayer) && array[3].equals(currentPlayer) && array[6].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// column 2 winning condition
 		} else if (array[1].equals(currentPlayer) && array[4].equals(currentPlayer) && array[7].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// column 3 winning condition
 		} else if (array[2].equals(currentPlayer) && array[5].equals(currentPlayer) && array[8].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// diagonal winning condition
 		} else if (array[0].equals(currentPlayer) && array[4].equals(currentPlayer) && array[8].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
+			
+		// reverse diagonal winning condition
 		} else if (array[2].equals(currentPlayer) && array[4].equals(currentPlayer) && array[6].equals(currentPlayer)) {
-			System.out.printf("The %s player has won!\n", player);
+			System.out.println(winningMessage);
 			exit();
 		}
 	}
 
+	/**
+	 * Ends the current game by setting the instance variable inProgress to false.
+	 */
+	
 	public void exit() {
 		displayBoard();
-		this.on = false;
+		this.inProgress = false;
+	}
+
+	/**
+	 * Asks whether the user wishes to continue and validates the user's
+	 * choice to make sure that it is a valid choice. It only verifies 
+	 * whether the user's choice is "y" or "Y" to continue, otherwise
+	 * the game ends and message is displayed notifying the user that the
+	 * game has ended.
+	 */
+	
+	public void checkWhetherToContinue() {
+
+		String userInput;
+
+		System.out.println("Would you like to continue?Press y/n.");
+		userInput = input.nextLine();
+
+		userInput = validateToContinue(userInput);
+
+		if (!(userInput.equalsIgnoreCase("y"))) {
+
+			this.on = false;
+
+			System.out.println("Thank you for playing. Goodbye.");
+		}
+	}
+
+	/**
+	 * Validates the user's choice to continue the game. It verifies
+	 * that the choice entered is only one character and only accepts 
+	 * the following characters: 'y', 'Y', 'n', and 'N'
+	 * 
+	 * @param userInput - the user's input collected as a String.
+	 * 
+	 * @return - the user's choice consisting of only one of the
+	 * accepted characters as a String.
+	 */
+	
+	public String validateToContinue(String userInput) {
+
+		int userInputLength = userInput.length();
+		int counter = 0;
+
+		while (userInputLength == 0 || userInputLength > 1) {
+
+			System.out.println("This is an invalid entry.");
+			System.out.println("Please choose a number y/n to continue.");
+			userInput = input.nextLine();
+			userInputLength = userInput.length();
+		}
+
+		while (counter < userInputLength) {
+			if (!(userInput.charAt(counter) == 'y') && !(userInput.charAt(counter) == 'Y')
+					&& !(userInput.charAt(counter) == 'n') && !(userInput.charAt(counter) == 'N')) {
+				System.out.println("This is an invalid entry.");
+				System.out.println("Please choose a number y/n to continue.");
+				userInput = input.nextLine();
+				userInputLength = userInput.length();
+				counter = 0;
+			} else {
+				counter++;
+			}
+			while (userInputLength == 0 || userInputLength > 1) {
+				System.out.println("This is an invalid entry.");
+				System.out.println("Please choose a number y/n to continue.");
+				userInput = input.nextLine();
+				userInputLength = userInput.length();
+			}
+		}
+
+		return userInput;
 	}
 
 	/**
@@ -225,6 +389,7 @@ class TicTacToeV2 {
 	 *            a String for validation.
 	 * @return a String consisting of only letters.
 	 */
+	
 	public String validateUserName(String userInput) {
 
 		int userInputLength = userInput.length();
@@ -257,7 +422,19 @@ class TicTacToeV2 {
 		}
 
 		return userInput;
-	} // end of method
+	} 
+	
+	/**
+	 * Validates the player's choice for position on the board. The only
+	 * choices allowed are integers from 0 to 8 inclusive. It does not allow
+	 * any spaces, special characters, or alphabetic characters. In addition
+	 * to only accepting integers 0 to 8, it only allows for one integer to be
+	 * entered as a choice.
+	 * 
+	 * @param userInput - the user's choice as a String.
+	 * 
+	 * @return - the user's choice represented as an integer.
+	 */
 
 	public int validatePlayersMove(String userInput) {
 
@@ -296,6 +473,6 @@ class TicTacToeV2 {
 		}
 		index = Integer.parseInt(userInput);
 		return index;
-	} // end of method
+	}
 
 } // end of TicTacToe
